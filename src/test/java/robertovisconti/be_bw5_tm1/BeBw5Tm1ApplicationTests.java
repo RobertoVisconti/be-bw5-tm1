aa34a593f66d417254e31f4d975d2e25130a8592
Roberto Visconti <robertovisconti93@gmail.com>
2026-07-20 11:29:31 +0200

    Initial commit

---
+++ src/test/java/robertovisconti/be_bw5_tm1/BeBw5Tm1ApplicationTests.java
@@ -0,0 +1,13 @@
+package robertovisconti.be_bw5_tm1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeBw5Tm1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
