1e6fbff6d1ef1594847eb9020947984eaf013f4e
Tancredi <tancredi.dangelo.22@gmail.com>
2026-07-21 15:51:54 +0200

    Update IdStatoFattura
    Completed Entity and fixed GenerationType error

--- src/main/java/robertovisconti/be_bw5_tm1/entities/IdStatoFattura.java
+++ src/main/java/robertovisconti/be_bw5_tm1/entities/IdStatoFattura.java
@@ -1,15 +1,27 @@
 package robertovisconti.be_bw5_tm1.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
 
 @Entity
 @Table
 public class IdStatoFattura {
 
+    // attributes
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Setter(AccessLevel.NONE)
     private UUID id;
 
     @Column(unique = true)
@@ -19,4 +31,22 @@ public class IdStatoFattura {
     private String descrizione;
 
 
+    // constructor
+
+    public IdStatoFattura(int titolo, String descrizione) {
+        this.titolo = titolo;
+        this.descrizione = descrizione;
+    }
+
+
+    // to string
+
+    @Override
+    public String toString() {
+        return "IdStatoFattura{" +
+                "id=" + id +
+                ", titolo=" + titolo +
+                ", descrizione='" + descrizione + '\'' +
+                '}';
+    }
 }
