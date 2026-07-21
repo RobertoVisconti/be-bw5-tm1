0bb44e150e66f33d7be1d0d629bfcda6b8a0c345
Roberto Visconti <robertovisconti93@gmail.com>
2026-07-21 10:40:01 +0200

    create entity Cliente
    aggiunto entity cliente e cartella controller

---
+++ src/main/java/robertovisconti/be_bw5_tm1/enums/TipoCliente.java
@@ -0,0 +1,5 @@
+package robertovisconti.be_bw5_tm1.enums;
+
+public enum TipoCliente {
+    PA, SAS, SPA, SRL
+}
---
+++ src/main/java/robertovisconti/be_bw5_tm1/payloadsDTO/ClienteDTO.java
@@ -0,0 +1,4 @@
+package robertovisconti.be_bw5_tm1.payloadsDTO;
+
+public class ClienteDTO {
+}
