7a2a23ce2dabbd7b5c5ed5c5eca3c60b003210f9
Tancredi <tancredi.dangelo.22@gmail.com>
2026-07-22 12:08:53 +0200

    Update StatoFattura.java

--- src/main/java/robertovisconti/be_bw5_tm1/entities/StatoFattura.java
+++ src/main/java/robertovisconti/be_bw5_tm1/entities/StatoFattura.java
@@ -1,22 +1,44 @@
 package robertovisconti.be_bw5_tm1.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
 
 @Entity
 @Table(name = "stato_fattura")
 public class StatoFattura {
+
+    // sttributes
 
     @Id
     @GeneratedValue
     private UUID id;
 
     @Column(unique = true)
-    private int titolo;
-
-    @Column
-    private String descrizione;
+    private String titolo;
 
 
+    // constructor
+
+    public StatoFattura(String titolo) {
+        this.titolo = titolo;
+    }
+
+
+    // to string
+
+    @Override
+    public String toString() {
+        return "StatoFattura{" +
+                "id=" + id +
+                ", titolo='" + titolo + '\'' +
+                '}';
+    }
 }
