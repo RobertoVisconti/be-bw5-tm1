eb7f5e428c7433a955544ba637f894f20478ce42
Tancredi <tancredi.dangelo.22@gmail.com>
2026-07-22 10:00:34 +0200

    Update FatturaRepository.java

--- src/main/java/robertovisconti/be_bw5_tm1/repositories/FatturaRepository.java
+++ src/main/java/robertovisconti/be_bw5_tm1/repositories/FatturaRepository.java
@@ -2,8 +2,6 @@ package robertovisconti.be_bw5_tm1.repositories;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
