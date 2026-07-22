558a46c86664b7f2b0917cf938e468d592518c72
Tancredi <tancredi.dangelo.22@gmail.com>
2026-07-22 12:08:57 +0200

    Create FatturaSpecification.java

---
+++ src/main/java/robertovisconti/be_bw5_tm1/specifications/FatturaSpecification.java
@@ -0,0 +1,13 @@
+package robertovisconti.be_bw5_tm1.specifications;
+
+import org.springframework.data.jpa.domain.Specification;
+import robertovisconti.be_bw5_tm1.entities.Fattura;
+
+import java.util.UUID;
+
+public class FatturaSpecification {
+
+    public Specification<Fattura> hasIdCliente(UUID uuid) {
+        return (root, query)
+    }
+}
