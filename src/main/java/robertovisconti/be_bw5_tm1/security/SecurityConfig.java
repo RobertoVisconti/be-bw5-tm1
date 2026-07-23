2e09793a5ba1dbdd16984a60c5b199051c9762ea
Gianni Bussoletti <BussolettiGianni@gmail.com>
2026-07-23 12:07:06 +0200

    aggiunto cors filter

--- src/main/java/robertovisconti/be_bw5_tm1/security/SecurityConfig.java
+++ src/main/java/robertovisconti/be_bw5_tm1/security/SecurityConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -40,4 +45,15 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder(12);
     }
 
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
 }
